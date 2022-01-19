e8e16b09da3fee4c1539bb25968623d49ea1b2c0
ZakSchenck <91504668+ZakSchenck@users.noreply.github.com>
2022-01-19 16:39:43 -0500

    Removed bulk of un needed code

--- password.js
+++ password.js
@@ -19,60 +19,22 @@ function generate() {
     let passwordLength;
     passwordLength = slider.value;
     let password = "";
-    // numbers for everything checked
-    if (numbersCheckbox.checked && uppercaseCheckbox.checked && lowercaseCheckbox.checked) {
-        let allCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        for (let i = 0; i < passwordLength; i++) {
-            let randomNumber = Math.floor(Math.random() * allCharacters.length);
-            password += allCharacters.substring(randomNumber, randomNumber + 1);
-        }
+    let allowedCharacters = '';
+    // Logic for number checked
+    if (numbersCheckbox.checked) {
+        allowedCharacters += '0123456789';
     }
-    // logic for numbers and uppercase letters checked
-    else if (numbersCheckbox.checked && uppercaseCheckbox.checked) {
-        let numbersAndUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-        for (let i = 0; i < passwordLength; i++) {
-            let randomNumber = Math.floor(Math.random() * numbersAndUppercase.length);
-            password += numbersAndUppercase.substring(randomNumber, randomNumber + 1);
-        }
+    // Logic for uppercase checked
+    if (uppercaseCheckbox.checked) {
+        allowedCharacters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     }
-    // Logic for numbers and lowercase letters checked
-    else if (numbersCheckbox.checked && lowercaseCheckbox.checked) {
-        let numbersAndLowercase = 'abcdefghijklmnopqrstuvwxyz0123456789';
-        for (let i = 0; i < passwordLength; i++) {
-            let randomNumber = Math.floor(Math.random() * numbersAndLowercase.length);
-            password += numbersAndLowercase.substring(randomNumber, randomNumber + 1);
-        }
-    } // Logic for uppercase and lowercase checked
-    else if (uppercaseCheckbox.checked && lowercaseCheckbox.checked) {
-        let uppercaseAndLowercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-        for (let i = 0; i < passwordLength; i++) {
-            let randomNumber = Math.floor(Math.random() * uppercaseAndLowercase.length);
-            password += uppercaseAndLowercase.substring(randomNumber, randomNumber + 1);
-        }
+    // Logic for lowercase checked
+    if (lowercaseCheckbox.checked) {
+        allowedCharacters += 'abcdefghijklmnopqrstuvwxyz';
     }
-    // Logic for only numbers checked
-    else if (numbersCheckbox.checked) {
-        let numbers = '0123456789';
-        for (let i = 0; i < passwordLength; i++) {
-            let randomNumber = Math.floor(Math.random() * numbers.length);
-            password += numbers.substring(randomNumber, randomNumber + 1);
-        }
-    }
-    // Logic for only uppercase checked
-    else if (uppercaseCheckbox.checked) {
-        let uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        for (let i = 0; i < passwordLength; i++) {
-            let randomNumber = Math.floor(Math.random() * uppercaseCharacters.length);
-            password += uppercaseCharacters.substring(randomNumber, randomNumber + 1);
-        }
-    }
-    // Logic for only lowercase checked
-    else if (lowercaseCheckbox.checked) {
-        let lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
-        for (let i = 0; i < passwordLength; i++) {
-            let randomNumber = Math.floor(Math.random() * lowercaseCharacters.length);
-            password += lowercaseCharacters.substring(randomNumber, randomNumber + 1);
-        }
+    for (let i = 0; i < passwordLength; i++) {
+        let randomNumber = Math.floor(Math.random() * allowedCharacters.length);
+        password += allowedCharacters.substring(randomNumber, randomNumber + 1);
     }
     document.getElementById("result").innerText = password;
 }
